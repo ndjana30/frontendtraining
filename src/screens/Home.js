041fac2c12c1f2569ca0feb0080b8a8cc87c4c06
ndjana30 <davyndjana@gmail.com>
2024-01-01 17:40:15 +0100

    new commit

---
+++ src/screens/Home.js
@@ -0,0 +1,44 @@
+import { Outlet, Link } from "react-router-dom";
+export default function Home()
+{
+    return(
+        <body>
+      
+    <header class="header">
+        <div class="logo">
+           <a href="#" class="logo-text"> TURNER </a>
+        </div>
+    
+        <div class="navigation">
+            <ul class="items">
+                <li><Link to="/"><a>Home</a></Link></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Pricing</a></li>
+            </ul>
+        </div>
+      </header>
+      <section class="pic-n-short-about">
+       
+        
+        <div class="short-about">
+          <span class="about-text">My name in NDJANA ESSAGA GERMAIN DAVY,</span>
+          <span class="about-text">born in yaounde on the 16th of february XXXX.</span>
+          <span class="about-text"> I went though an anglophone system of education and had my GCE ADVANCED LEVEL.</span>
+          <span class="about-text">At the university level, i did software engineering at the ICT UNIVERSITY.</span>
+          <span class="about-text"> After 4 years, i had a bachelors degree of science in software engineering.</span> 
+          <span class="about-text">Today, i am working as fullstack software engineer at ALPHA DIGITAL SERVICES.</span>
+        </div>
+        
+        <div className="start-button">
+        
+          <a class="started" href="#">Get Started</a>
+          
+        </div>
+        
+      </section>
+
+      
+</body>
+    )
+}
